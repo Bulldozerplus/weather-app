c6102e947124434a2d7e13312611e56577dcdcf8
Taldykin Nikita <nikitataldykin3156@yandex.ru>
2023-07-18 16:41:03 +0300

    Refactor code

--- src/App.js
+++ src/App.js
@@ -6,58 +6,83 @@ function App() {
 
     const [data, setData] = useState({})
     const [location, setLocation] = useState('Moscow')
+    const [state, setState] = useState('blank')
+
 
     async function fetchWeather() {
         try {
-        const urlWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=61eae1b135251c95a1d8f7d060d50322`)
-            .then((response => setData(response.data)))}
-        catch (err){
-
+            setState('loading')
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=61eae1b135251c95a1d8f7d060d50322`)
+            setData(response.data)
+            setState('success')
+        } catch (err) {
+            setState('error')
         }
     }
 
     useEffect(() => {
-        setData(fetchWeather)
-    },[location])
-    console.log(data)
+        fetchWeather()
+    }, [])
 
-    return (
-        <div className="App">
-            <div className='search'>
-                <input value={location}
-                       type='text'
-                       placeholder='Enter your location'
-                       onChange={event => setLocation(event.target.value)}/>
-            </div>
+    function detectKeyPress(e) {
+        if (e.key === 'Enter') {
+            return fetchWeather()
+        }
+    }
+
+    if (state === 'loading') {
+        return <div className='App'>
             <div className='container'>
-                <div className='top'>
-                    <div className='location'>
-                        <p>{data.name}</p>
-                    </div>
-                    <div className='temp'>
-                        {data.main ? <h1 className='bold'>{data.main.temp}</h1> : null}
-                    </div>
-                    <div className='description'>
-                        {data.weather ? <p>{data.weather[0].main}</p> : null}
-                    </div>
-                </div>
-                <div className='bottom'>
-                    <div className='feels'>
-                        {data.main ? <p className='bold'>{data.main.feels_like}</p> : null}
-                        <p>Fells like</p>
-                    </div>
-                    <div className='humidity'>
-                        {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
-                        <p>Humidity</p>
-                    </div>
-                    <div className='wind'>
-                        {data.wind ? <p className='bold'>{data.wind.speed}</p> : null}
-                        <p>Wind speed</p>
-                    </div>
-                </div>
+                <h2 className='bold' className='search'>Loading. Please wait.</h2>
             </div>
         </div>
-    );
+    } else if (state === 'error') {
+        return <div className='App'>
+            <div className='container'>
+                <h2 className='bold' className='search'>Error, please update this page</h2>
+            </div>
+        </div>
+    } else if (state === 'success') {
+        return (
+            <div className="App">
+                <div className='search'>
+                    <input value={location}
+                           type='text'
+                           placeholder='Enter your location'
+                           onChange={event => setLocation(event.target.value)}
+                           onKeyPress={detectKeyPress}
+                    />
+                </div>
+                <div className='container'>
+                    <div className='top'>
+                        <div className='location'>
+                            <p>{data.name}</p>
+                        </div>
+                        <div className='temp'>
+                            <h1 className='bold'>{data.main.temp} C</h1>
+                        </div>
+                        <div className='description'>
+                            <p>{data.weather[0].main}</p>
+                        </div>
+                    </div>
+                    <div className='bottom'>
+                        <div className='feels'>
+                            <p className='bold'>{data.main.feels_like} C</p>
+                            <p>Fells like</p>
+                        </div>
+                        <div className='humidity'>
+                            <p className='bold'>{data.main.humidity} %</p>
+                            <p>Humidity</p>
+                        </div>
+                        <div className='wind'>
+                            <p className='bold'>{data.wind.speed} M/S</p>
+                            <p>Wind speed</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
