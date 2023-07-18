96e8f6311c3b60ed5c1cecc2d7788f5fb1d4495f
Taldykin Nikita <nikitataldykin3156@yandex.ru>
2023-07-18 11:42:26 +0300

    Create functional and logic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('Moscow')
+
+    async function fetchWeather() {
+        try {
+        const urlWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=61eae1b135251c95a1d8f7d060d50322`)
+            .then((response => setData(response.data)))}
+        catch (err){
+
+        }
+    }
+
+    useEffect(() => {
+        setData(fetchWeather)
+    },[location])
+    console.log(data)
+
+    return (
+        <div className="App">
+            <div className='search'>
+                <input value={location}
+                       type='text'
+                       placeholder='Enter your location'
+                       onChange={event => setLocation(event.target.value)}/>
+            </div>
+            <div className='container'>
+                <div className='top'>
+                    <div className='location'>
+                        <p>{data.name}</p>
+                    </div>
+                    <div className='temp'>
+                        {data.main ? <h1 className='bold'>{data.main.temp}</h1> : null}
+                    </div>
+                    <div className='description'>
+                        {data.weather ? <p>{data.weather[0].main}</p> : null}
+                    </div>
+                </div>
+                <div className='bottom'>
+                    <div className='feels'>
+                        {data.main ? <p className='bold'>{data.main.feels_like}</p> : null}
+                        <p>Fells like</p>
+                    </div>
+                    <div className='humidity'>
+                        {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
+                        <p>Humidity</p>
+                    </div>
+                    <div className='wind'>
+                        {data.wind ? <p className='bold'>{data.wind.speed}</p> : null}
+                        <p>Wind speed</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
